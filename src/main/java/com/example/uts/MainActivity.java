37469d58e0c46648f1cb8392f57ab22cec5db9e2
BrendanCicero <92319255+BrendanCicero@users.noreply.github.com>
2022-04-22 23:51:11 +0700

    Add files via upload
    UTS - Pemrograman Perangkat Bergerak

---
+++ src/main/java/com/example/uts/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.uts;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String[] s1, s2;
+    int[] images = {R.drawable.amaris, R.drawable.anakta,R.drawable.belanova,R.drawable.clarkson, R.drawable.doxtron,R.drawable.goudevard,R.drawable.granium,R.drawable.rumbert, R.drawable.swisselish,R.drawable.watson};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.hotel);
+        s2 = getResources().getStringArray(R.array.description);
+
+        MyAdapter myAdapter = new MyAdapter(this,s1,s2,images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
